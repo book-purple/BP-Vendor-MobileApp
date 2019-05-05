2edfff25fdfb1fb6e3a6d9915b0b53684ecf2ff1
gaurav4ever <gauravsharma.mvp@gmail.com>
2019-05-05 12:17:24 +0530

    files added:

---
+++ app/src/main/java/com/bookpurple/bookpurplevendor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bookpurple.bookpurplevendor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
